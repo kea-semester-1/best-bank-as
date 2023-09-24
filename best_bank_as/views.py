9dd6e5c7d13e4f2c1dbf2507bef9c6ba0ab40b6b
martinlaursen97 <martin_laursen21@hotmail.com>
2023-09-24 16:19:27 +0200

    test

--- best_bank_as/views.py
+++ best_bank_as/views.py
@@ -2,6 +2,11 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    """View for index."""
+def index(request) -> HttpResponse:
+    """View for index"""
+
+
+
+    
+
     return render(request, "best_bank_as/index.html")
