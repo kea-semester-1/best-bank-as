37d358332c460ed3489384a6f4df34b7dc6a0d06
Moshizzl3 <81045658+Moshizzl3@users.noreply.github.com>
2023-11-30 15:01:39 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -153,3 +153,7 @@ STATIC_ROOT = "/static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+SECURE_HSTS_SECONDS = 3600  
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
