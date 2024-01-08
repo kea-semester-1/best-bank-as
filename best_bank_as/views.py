b5d9b2ded4d1d663ed51851d62ad5f780c744357
Malthe <malthegram22@gmail.com>
2024-01-08 12:48:43 +0100

    linter

--- best_bank_as/views.py
+++ best_bank_as/views.py
@@ -21,226 +21,226 @@ status_list = [(status.name, status.value) for status in AccountStatus]
 
 
 def index(request: HttpRequest) -> HttpResponse:
-	"""View for index."""
-	return render(request, "best_bank_as/index.html")
+    """View for index."""
+    return render(request, "best_bank_as/index.html")
 
 
 # TODO: Is this the correct way to ensure user can only see his own page
 @login_required
 def profile_page(request: HttpRequest, username: str) -> HttpResponse:
-	"""View for a user's profile page."""
-	user = get_object_or_404(User, username=username)
-	
-	customer = get_object_or_404(Customer, user=user)
-	
-	if request.user != user:
-		return HttpResponseForbidden(
-			render(request, "best_bank_as/error_pages/error_page.html")
-		)
-	
-	context = {"customer": customer}
-	
-	return render(request, "best_bank_as/profile.html", context)
+    """View for a user's profile page."""
+    user = get_object_or_404(User, username=username)
+
+    customer = get_object_or_404(Customer, user=user)
+
+    if request.user != user:
+        return HttpResponseForbidden(
+            render(request, "best_bank_as/error_pages/error_page.html")
+        )
+
+    context = {"customer": customer}
+
+    return render(request, "best_bank_as/profile.html", context)
 
 
 @login_required
 def get_accounts_list(request: HttpRequest) -> HttpResponse:
-	"""Retrieve all accounts for a given user."""
-	
-	if request.method == "GET":
-		customer = get_object_or_404(Customer, user=request.user)
-		accounts = customer.get_accounts()
-		context = {"accounts": accounts, "status_list": status_list}
-	
-	if request.method == "POST":
-		form = NewAccountRequestForm(request.POST)
-		if form.is_valid():
-			new_account = Account.request_new_account(customer=request.user.customer)
-			response_text = (
-				f"Status: {new_account.account_status}, "
-				f"Account number: {new_account.account_number}"
-			)
-			context = {"data": response_text}
-			return render(
-				request, "best_bank_as/accounts/request_account_partial.html", context
-			)
-	
-	return render(request, "best_bank_as/accounts/account_list.html", context)
+    """Retrieve all accounts for a given user."""
+
+    if request.method == "GET":
+        customer = get_object_or_404(Customer, user=request.user)
+        accounts = customer.get_accounts()
+        context = {"accounts": accounts, "status_list": status_list}
+
+    if request.method == "POST":
+        form = NewAccountRequestForm(request.POST)
+        if form.is_valid():
+            new_account = Account.request_new_account(customer=request.user.customer)
+            response_text = (
+                f"Status: {new_account.account_status}, "
+                f"Account number: {new_account.account_number}"
+            )
+            context = {"data": response_text}
+            return render(
+                request, "best_bank_as/accounts/request_account_partial.html", context
+            )
+
+    return render(request, "best_bank_as/accounts/account_list.html", context)
 
 
 @login_required
 def get_account_details(request: HttpRequest, pk: int) -> HttpResponse:
-	"""Retrieve information for a given account."""
-	account = get_object_or_404(Account, pk=pk)
-	
-	context = {"account": account, "status_list": status_list}
-	
-	if request.user != account.customer.user and not request.user.is_staff:
-		return HttpResponseForbidden(
-			render(request, "best_bank_as/error_pages/error_page.html")
-		)
-	
-	if request.method == "GET":
-		balance = account.get_balance()
-		transactions = account.get_transactions()
-		
-		context = {"balance": balance, "transactions": transactions}
-	
-	if request.method == "PUT" and request.user.is_staff:
-		data = request.PUT
-		value = data.get("account_status")
-		try:
-			account.update_account_status(value)
-			account.refresh_from_db()
-			messages.success(request, "Account status was successfully updated.")
-		except Exception:  # TODO: Find more specific error
-			messages.error(request, "Something went wrong. Please try again.")
-		
-		return render(
-			request, "best_bank_as/accounts/account_status_partial.html", context
-		)
-	return render(request, "best_bank_as/accounts/account_details.html", context)
+    """Retrieve information for a given account."""
+    account = get_object_or_404(Account, pk=pk)
+
+    context = {"account": account, "status_list": status_list}
+
+    if request.user != account.customer.user and not request.user.is_staff:
+        return HttpResponseForbidden(
+            render(request, "best_bank_as/error_pages/error_page.html")
+        )
+
+    if request.method == "GET":
+        balance = account.get_balance()
+        transactions = account.get_transactions()
+
+        context = {"balance": balance, "transactions": transactions}
+
+    if request.method == "PUT" and request.user.is_staff:
+        data = request.PUT
+        value = data.get("account_status")
+        try:
+            account.update_account_status(value)
+            account.refresh_from_db()
+            messages.success(request, "Account status was successfully updated.")
+        except Exception:  # TODO: Find more specific error
+            messages.error(request, "Something went wrong. Please try again.")
+
+        return render(
+            request, "best_bank_as/accounts/account_status_partial.html", context
+        )
+    return render(request, "best_bank_as/accounts/account_details.html", context)
 
 
 @login_required
 def staff_page(request: HttpRequest, username: str) -> HttpResponse:
-	"""View for a staff page."""
-	user = get_object_or_404(User, username=username)
-	
-	if request.user != user:
-		return HttpResponseForbidden(
-			render(request, "best_bank_as/error_pages/error_page.html")
-		)
-	context = {user: user}
-	
-	return render(request, "best_bank_as/staff.html", context)
+    """View for a staff page."""
+    user = get_object_or_404(User, username=username)
+
+    if request.user != user:
+        return HttpResponseForbidden(
+            render(request, "best_bank_as/error_pages/error_page.html")
+        )
+    context = {user: user}
+
+    return render(request, "best_bank_as/staff.html", context)
 
 
 @login_required
 def search_customer(request: HttpRequest) -> HttpResponse:
-	"""View for searching customers."""
-	query = request.GET.get("query", "")
-	
-	if not request.user.is_staff:
-		return HttpResponseForbidden()
-	
-	customers = (
-		Customer.objects.filter(
-			Q(phone_number__icontains=query)
-			| Q(user__username__icontains=query)
-			| Q(account__account_number__icontains=query)
-		)
-		.distinct()
-		.select_related("user")  # join
-		.prefetch_related(
-			Prefetch(
-				"account_set",
-			)
-		)
-	)
-	
-	context = {"customers": customers, "query": query, "status_list": status_list}
-	return render(request, "best_bank_as/customers/customers_search.html", context)
+    """View for searching customers."""
+    query = request.GET.get("query", "")
+
+    if not request.user.is_staff:
+        return HttpResponseForbidden()
+
+    customers = (
+        Customer.objects.filter(
+            Q(phone_number__icontains=query)
+            | Q(user__username__icontains=query)
+            | Q(account__account_number__icontains=query)
+        )
+        .distinct()
+        .select_related("user")  # join
+        .prefetch_related(
+            Prefetch(
+                "account_set",
+            )
+        )
+    )
+
+    context = {"customers": customers, "query": query, "status_list": status_list}
+    return render(request, "best_bank_as/customers/customers_search.html", context)
 
 
 @login_required
 def new_loan_application(request: HttpRequest) -> HttpResponse:
-	"""View for creating a new loan application."""
-	
-	customer = get_object_or_404(Customer, user=request.user)
-	
-	if request.method == "POST":
-		if not customer.can_loan:
-			return render(
-				request,
-				"best_bank_as/error_pages/error_page.html",
-			)
-		
-		form = LoanApplicationForm(data=request.POST, customer=customer)
-		if not form.is_valid():
-			return render(
-				request,
-				"best_bank_as/error_pages/error_page.html",
-			)
-		form_data = form.cleaned_data
-		application = CustomerApplication(
-			reason=form_data["reason"],
-			amount=form_data["amount"],
-			type=ApplicationType.LOAN,
-			status=ApplicationStatus.PENDING,
-			customer=customer,
-		)
-		application.save()
-		return redirect("best_bank_as:new_loan_application")
-	
-	context = {
-		"customer": customer,
-		"loan_applications": customer.loan_applications,
-		"form": LoanApplicationForm(customer=customer),
-	}
-	
-	return render(
-		request,
-		"best_bank_as/loans/loan_application.html",
-		context,
-	)
+    """View for creating a new loan application."""
+
+    customer = get_object_or_404(Customer, user=request.user)
+
+    if request.method == "POST":
+        if not customer.can_loan:
+            return render(
+                request,
+                "best_bank_as/error_pages/error_page.html",
+            )
+
+        form = LoanApplicationForm(data=request.POST, customer=customer)
+        if not form.is_valid():
+            return render(
+                request,
+                "best_bank_as/error_pages/error_page.html",
+            )
+        form_data = form.cleaned_data
+        application = CustomerApplication(
+            reason=form_data["reason"],
+            amount=form_data["amount"],
+            type=ApplicationType.LOAN,
+            status=ApplicationStatus.PENDING,
+            customer=customer,
+        )
+        application.save()
+        return redirect("best_bank_as:new_loan_application")
+
+    context = {
+        "customer": customer,
+        "loan_applications": customer.loan_applications,
+        "form": LoanApplicationForm(customer=customer),
+    }
+
+    return render(
+        request,
+        "best_bank_as/loans/loan_application.html",
+        context,
+    )
 
 
 @login_required
 def loan_applications_list(request: HttpRequest) -> HttpResponse:
-	"""View for listing all loan applications."""
-	...
+    """View for listing all loan applications."""
+    ...
 
 
 @login_required
 # @require_http_methods(["GET", "DELETE"])
 def loan_application_details(request: HttpRequest, pk: int) -> HttpResponse:
-	"""Retrieve information for a given loan application."""
-	
-	customer = get_object_or_404(Customer, user=request.user)
-	application = get_object_or_404(CustomerApplication, pk=pk)
-	if application.customer != customer:
-		return HttpResponseForbidden(
-			render(request, "best_bank_as/error_pages/error_page.html")
-		)
-	
-	if request.method == "DELETE":
-		application.delete()
-		return redirect("best_bank_as:new_loan_application")
-	
-	context = {
-		"loan_application": application,
-		"status_name": application.status_name,
-	}
-	
-	return render(
-		request,
-		"best_bank_as/loans/loan_application_details.html",
-		context,
-	)
+    """Retrieve information for a given loan application."""
+
+    customer = get_object_or_404(Customer, user=request.user)
+    application = get_object_or_404(CustomerApplication, pk=pk)
+    if application.customer != customer:
+        return HttpResponseForbidden(
+            render(request, "best_bank_as/error_pages/error_page.html")
+        )
+
+    if request.method == "DELETE":
+        application.delete()
+        return redirect("best_bank_as:new_loan_application")
+
+    context = {
+        "loan_application": application,
+        "status_name": application.status_name,
+    }
+
+    return render(
+        request,
+        "best_bank_as/loans/loan_application_details.html",
+        context,
+    )
 
 
 @login_required()
 def transfer_money(request: HttpRequest) -> HttpResponse:  # TODO: Transaction naming
-	"""View to transfer money from account to account."""
-	if request.method != "POST":
-		return render(
-			request,
-			"best_bank_as/handle_funds/transfer-money.html",
-			{"form": TransferForm(user=request.user)},
-		)
-	form = TransferForm(data=request.POST, user=request.user)
-	if not form.is_valid():  # or throw exception
-		return render(
-			request,
-			"best_bank_as/handle_funds/transfer-money.html",
-			{"form": form},
-		)
-	source_account = form.cleaned_data["source_account"]
-	destination_account = form.cleaned_data["destination_account"]
-	destination_account_instance = Account.objects.get(
-		account_number=destination_account
-	)
-	amount = form.cleaned_data["amount"]
-	Ledger.transfer(source_account, destination_account_instance, amount)
-	return redirect("best_bank_as:index")
+    """View to transfer money from account to account."""
+    if request.method != "POST":
+        return render(
+            request,
+            "best_bank_as/handle_funds/transfer-money.html",
+            {"form": TransferForm(user=request.user)},
+        )
+    form = TransferForm(data=request.POST, user=request.user)
+    if not form.is_valid():  # or throw exception
+        return render(
+            request,
+            "best_bank_as/handle_funds/transfer-money.html",
+            {"form": form},
+        )
+    source_account = form.cleaned_data["source_account"]
+    destination_account = form.cleaned_data["destination_account"]
+    destination_account_instance = Account.objects.get(
+        account_number=destination_account
+    )
+    amount = form.cleaned_data["amount"]
+    Ledger.transfer(source_account, destination_account_instance, amount)
+    return redirect("best_bank_as:index")
