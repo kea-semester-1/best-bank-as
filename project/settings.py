f0aa0f45cf1575c656dc953729fac5703181f6d2
mslaursen <martin_laursen21@hotmail.com>
2024-01-23 00:45:12 +0100

    feature

--- project/settings.py
+++ project/settings.py
@@ -156,3 +156,12 @@ SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = ["https://what-lol.dk", "https://www.what-lol.dk"]
+
+# Email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+DEFAULT_FROM_EMAIL = os.environ["DEFAULT_FROM_EMAIL"]
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
